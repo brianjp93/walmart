a45061e617a4bed7b8e1ba72b600c8d0e6c7c587
Brianjp93 <brianjp93@gmail.com>
2017-02-15 16:09:30 -0800

    Add functionality, include requirements.txt
    I've wrapped most of the methods in the api I think.  I haven't wrapped
    the bulk upload items because it is difficult to wrap well and I don't
    need it right now.  Also did not wrap order cancellation or refunds.

--- pymart.py
+++ pymart.py
@@ -1,5 +1,7 @@
 '''
 wrapper around the walmart api
+
+Walmart's API documentation -> https://developer.walmart.com/#/apicenter/marketPlace
 '''
 import requests
 from requests import Request, Session
@@ -14,6 +16,7 @@ from uuid import uuid4
 from xml.etree import ElementTree as ET
 import pytz
 from datetime import datetime
+import zipfile
 try:
     # python 2
     from StringIO import StringIO
@@ -24,6 +27,16 @@ except:
 
 __author__ = 'Brian Perrett'
 __date__   = 'Feb 10, 2017'
+
+
+def clean_report(data_string):
+    '''
+    returns a string of the report
+    '''
+    f = zipfile.ZipFile(StringIO(data_string))
+    filename = f.filelist[0].filename
+    data = f.read(filename)
+    return data
 
 
 class Walmart():
@@ -51,29 +64,26 @@ class Walmart():
     def sign(self, full_url, request_method):
         '''
         use the cryptography library to sign a specific string
-        returns a tuple as (signature[string], timestamp[milliseconds])
+        returns a tuple as (signature[string], timestamp[int in milliseconds])
         '''
         timestamp = str(int(time.time() * 1000))
         string_to_sign = '{consumer_id}\n{full_url}\n{http_method}\n{timestamp}\n'.format(**{
-                'consumer_id': self.consumer_id,
-                'full_url': full_url,
-                'http_method': request_method.upper(),
-                'timestamp': timestamp
-                })
-        # string_to_sign.encode('utf-8')
-        # print(string_to_sign)
+            'consumer_id': self.consumer_id,
+            'full_url': full_url,
+            'http_method': request_method.upper(),
+            'timestamp': timestamp
+            })
         pem_format = '-----BEGIN PRIVATE KEY-----\n{}\n-----END PRIVATE KEY-----'.format(self.private_key)
-        # print(pem_format)
         crypto_key = serialization.load_pem_private_key(
             pem_format,
             password=None,
             backend=default_backend()
-        )
+            )
         signature = crypto_key.sign(
             string_to_sign,
             padding.PKCS1v15(),
             hashes.SHA256()
-        )
+            )
         signature = base64.b64encode(signature)
         return signature, timestamp
 
@@ -113,6 +123,54 @@ class Walmart():
         signature, timestamp = self.sign(full_url, 'PUT')
         headers = self.get_headers(timestamp, signature)
         r = requests.put(full_url, headers=headers)
+        return r
+
+    def bulk_update_price(self, data=[], version='1.5', current_price_type_default='', currency_default='USD'):
+        '''
+        data is a list of dictionary objects which looks like...
+            [{
+                'sku':                  <sku>,
+                'currency':             <currency>,
+                'current_price':        <current_price>,
+                'current_price_type':   <current_price_type>,
+                'comparison_price':     <comparison_price>,
+                ''
+            }, ...]
+        currenct_price_type -> ('BASE', 'CLEARANCE', 'REDUCED')
+        '''
+        full_url = '{}v3/feeds?feedType=price'.format(self.base_url)
+        price_feed_elt = ET.Element('PriceFeed')
+        price_feed_elt.set('xmlns:gmp', 'http://walmart.com/')
+        price_header_elt = ET.SubElement(price_feed_elt, 'PriceHeader')
+        version_elt = ET.SubElement(price_header_elt, 'version')
+        version_elt.text = version
+        for item in data:
+            currency = str(item.get('currency', currency_default))
+            price_elt = ET.SubElement(price_feed_elt, 'Price')
+            item_identifier_elt = ET.SubElement(price_elt, 'itemIdentifier')
+            sku_elt = ET.SubElement(item_identifier_elt, 'sku')
+            sku_elt.text = item['sku']
+            pricing_list_elt = ET.SubElement(price_elt, 'pricingList')
+            pricing_elt = ET.SubElement(pricing_list_elt, 'pricing')
+            current_price_elt = ET.SubElement(pricing_elt, 'currentPrice')
+            current_price_value_elt = ET.SubElement(current_price_elt, 'value')
+            current_price_value_elt.set('currency', currency)
+            current_price_value_elt.set('amount', str(item['current_price']))
+            current_price_type = item.get('current_price_type', current_price_type_default)
+            if current_price_type:
+                current_price_type_elt = ET.SubElement(pricing_elt, 'currentPriceType')
+                current_price_type_elt.text = current_price_type
+            if item.get('comparison_price', None):
+                comparison_price_elt = ET.SubElement(pricing_elt, 'comparisonPrice')
+                comparison_price_value_elt = ET.SubElement(comparison_price_elt, 'value')
+                comparison_price_value_elt.set('currency', currency)
+                comparison_price_value_elt.set('amount', str(item['comparison_price']))
+        sig, ts = self.sign(full_url, 'POST')
+        headers = self.get_headers(ts, sig)
+        headers['Content-Type'] = 'multipart/form-data;'
+        xml_string = self.xml_head + ET.tostring(price_feed_elt)
+        files = {'file': ('pricing.xml', xml_string)}
+        r = requests.post(full_url, headers=headers, files=files)
         return r
 
     ###########################################################
@@ -173,33 +231,74 @@ class Walmart():
         r = requests.post(full_url, headers=headers)
         return r
 
-    def cancel_order_lines(self, purchase_order_id, line_number, status):
+    def cancel_order_lines(self, purchase_order_id, data=[]):
         '''
         https://developer.walmartapis.com/#cancelling-order-lines56
         '''
         full_url = '{}v2/orders/{}/cancel'.format(self.base_url, purchase_order_id)
+        # TODO: write it
 
-    def update_shipping(
-            self,
-            purchase_order_id,
-            line_number,
-            status,
-            ship_date_time,
-            carrier_name,
-            method_code,
-            tracking_number,
-            tracking_url=None
-            ):
+    def refund_order_lines(self, purchase_order_id):
+        '''
+        '''
+        full_url = '{}v3/orders/{}/refund'.format(self.base_url, purchase_order_id)
+        # TODO: write it
+
+    def update_shipping(self, purchase_order_id, data=[], status_default='Shipped', unit_of_measurement_default='EACH'):
         '''
         https://developer.walmartapis.com/#shipping-notificationsupdates58
+        data is a list of dictionary objects with the line items...
+            [{
+                'line_number':          <lineNumber>,
+                'status':               <status>,
+                'quantity':             <amount>,
+                'unit_of_measurement':  <unitOfMeasurement>,
+                'ship_date_time':       <shipDateTime>,
+                'carrier_name':         <carrierName>,
+                'method_code':          <methodCode>,
+                'tracking_number',      <trackingNumber>,
+                'tracking_url',         <trackingURL>
+            }, ...]
         '''
-        full_url = '{}/v2/orders/{}/shipping'.format(self.base_url, purchase_order_id)
+        full_url = '{}/v3/orders/{}/shipping'.format(self.base_url, purchase_order_id)
         ns2 = 'http://walmart.com/mp/orders'
         ns3 = 'http://walmart.com/'
-        order_shipment_elt = ET.Element('{}:orderShipment')
+        ns = 'ns2'
+        order_shipment_elt = ET.Element('{}:orderShipment'.format(ns))
         order_shipment_elt.set('xmlns:ns2', ns2)
         order_shipment_elt.set('xmlns:ns3', ns3)
-        # TODO: finish method.
+        order_lines_elt = ET.SubElement(order_shipment_elt, '{}:orderLines'.format(ns))
+        for item in data:
+            order_line_elt = ET.SubElement(order_lines_elt, '{}:orderLine'.format(ns))
+            line_number_elt = ET.SubElement(order_line_elt, '{}:lineNumber'.format(ns))
+            line_number_elt.text = item['line_number']
+            order_line_statuses_elt = ET.SubElement(order_line_elt, '{}:orderLineStatuses'.format(ns))
+            order_line_status_elt = ET.SubElement(order_line_statuses_elt, '{}:orderLineStatus'.format(ns))
+            status_elt = ET.SubElement(order_line_status_elt, '{}:status'.format(ns))
+            status_elt.text = item.get('status', status_default)
+            status_quantity_elt = ET.SubElement(order_line_status_elt, '{}:statusQuantity'.format(ns))
+            unit_of_measurement_elt = ET.SubElement(status_quantity_elt, '{}:unitOfMeasurement'.format(ns))
+            unit_of_measurement_elt.text = item.get('unit_of_measurement', unit_of_measurement_default)
+            amount_elt = ET.SubElement(status_quantity_elt, '{}:amount'.format(ns))
+            amount_elt.text = item['quantity']
+            tracking_info_elt = ET.SubElement(order_line_status_elt, '{}:trackingInfo'.format(ns))
+            ship_date_time_elt = ET.SubElement(tracking_info_elt, '{}:shipDateTime'.format(ns))
+            ship_date_time_elt.text = item['ship_date_time']
+            carrier_name_elt = ET.SubElement(tracking_info_elt, '{}:carrierName'.format(ns))
+            carrier_elt = ET.SubElement(carrier_name_elt, '{}:carrier'.format(ns))
+            method_code_elt = ET.SubElement(tracking_info_elt, '{}:methodCode'.format(ns))
+            method_code_elt.text = item['method_code']
+            tracking_number_elt = ET.SubElement(tracking_info_elt, '{}:trackingNumber'.format(ns))
+            tracking_number_elt.text = item['tracking_number']
+            if item.get('tracking_url', None):
+                tracking_url_elt = ET.SubElement(tracking_info_elt, '{}:trackingURL'.format(ns))
+                tracking_url_elt.text = item['tracking_url']
+        xml_string = self.xml_head + ET.tostring(order_shipment_elt)
+        sig, ts = self.sign(full_url, 'POST')
+        headers = self.get_headers(ts, sig)
+        headers['content-type'] = 'application/xml'
+        r = requests.post(full_url, headers=headers)
+        return r        
 
     ###############################################################
     ###################### INVENTORY METHODS ######################
@@ -271,4 +370,34 @@ class Walmart():
         headers = self.get_headers(timestamp, signature)
         headers['Content-Type'] = 'multipart/form-data;'
         r = requests.post(full_url, headers=headers, files=files)
+        return r
+
+    ###############################################################
+    ####################### REPORT METHODS ########################
+    ###############################################################
+
+    def get_item_report(self, cleaned=True):
+        '''
+        returns response object -> zipped csv file
+        if cleaned is set to True, then a tuple (response, data) is returned
+        '''
+        full_url = '{}v2/getReport?type=item'.format(self.base_url)
+        sig, ts = self.sign(full_url, 'GET')
+        headers = self.get_headers(ts, sig)
+        headers['Content-Type'] = 'application/xml'
+        r = requests.get(full_url, headers=headers)
+        if cleaned:
+            data = clean_report(r.content)
+            return r, data
+        return r
+
+    def get_buy_box_report(self, cleaned=True):
+        full_url = '{}v2/getReport?type=buybox'.format(self.base_url)
+        sig, ts = self.sign(full_url, 'GET')
+        headers = self.get_headers(ts, sig)
+        headers['Content-Type'] = 'application/xml'
+        r = requests.get(full_url, headers=headers)
+        if cleaned:
+            data = clean_report(r.content)
+            return r, data
         return r
